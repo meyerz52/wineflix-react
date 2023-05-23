45d2b50f0e25b58ba8ced9d8d8e67cc799f4c0c9
Zachary Meyer <Zachary.Meyer@libertymutual.com>
2023-05-23 14:25:42 -0700

    Add NavigationBar component and added to app

---
+++ src/Components/Navigation/NavigationBar.js
@@ -0,0 +1,19 @@
+import NavigationLinks from "./NavigationLinks";
+import Avatar from "./Avatar";
+
+const NavigationBar = ({ avatar = "?", links = [] }) => {
+
+    return (
+        <nav className="navigation">
+            <section className="primary-navigation">
+                <h1>"Wineflix</h1>
+                <NavigationLinks linkContext={links} />
+            </section>
+            <section className="secondary-navigation">
+                <Avatar Icon={avatar} />
+            </section>
+        </nav>
+    )
+}
+
+export default NavigationBar
