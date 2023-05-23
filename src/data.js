66d873f2717f3533a4fa6d729470eb0aaf4bc186
Zachary Meyer <Zachary.Meyer@libertymutual.com>
2023-05-23 14:27:15 -0700

    Add Data

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const navigationContext = [{
+    "title": "Home",
+    "href": "https://developer.mozilla.org"
+}, {
+    "title": "Whites",
+    "href": "https://developer.mozilla.org"
+}, {
+    "title": "Reds",
+    "href": "https://developer.mozilla.org"
+}, {
+    "title": "Blends",
+    "href": "https://developer.mozilla.org"
+}]
+
+
+export { navigationContext }
